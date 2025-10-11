977cb049bcafe27a484f6b04718eca88ec5508c7
Akhil <akil2k20@gmail.com>
2025-10-11 22:41:36 +0530

    hero jsx one word is added

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
             <div className="hero-content">
                 <h1 className="hero-title">Creating User-centric Digital Experiences that solve real problems</h1>
                 <p className="hero-description">
-                    I turn ideas into thoughtful, user-centerd products with a foucs on clarity and function. Focused on delivering user-centric solutions and enhancing technical expertise. 
+                    I turn ideas into thoughtful, user-centerd products with a foucs on clarity and functionality. Focused on delivering user-centric solutions and enhancing technical expertise. 
                 </p>
                 <div className="hero-buttons">
                     <Button text="View My Work" type="primary" className="btn"/>

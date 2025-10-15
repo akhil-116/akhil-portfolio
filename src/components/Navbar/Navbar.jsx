f37e6cd6a16f4c1ad079addaa70106751f754137
Akhil <akil2k20@gmail.com>
2025-10-15 13:19:59 +0530

    enabled hamburger menu in mobile using usestate

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,22 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from '../Button/Button';
 import "./Navbar.css";
 const Navbar = () => {
+
+     const [isOpen, setIsOpen] = useState(false);
+
+        const toggleMenu = () => {
+            setIsOpen(!isOpen);
+        };
+
+        const closeMenu = () => {
+            setIsOpen(false);
+        };
+
     return(
         <nav className="navbar">
             <div className="nav-container">
                 <h1 className="nav-logo">Akhil Dasari</h1>
 
-                <ul className="nav-links">
-                    <li><a href="#home">Home</a></li>
-                    <li><a href="#about">About</a></li>
-                    <li><a href="#projects">Projects</a></li>
-                    <li><a href="#contact">Contact</a></li>
+                <ul className={`nav-links ${isOpen ? "open" : ""}`}>
+                    <li><a href="/" className="active" onClick={closeMenu}>Home</a></li>
+                    <li><a href="#about" onClick={closeMenu}>About</a></li>
+                    <li><a href="#projects" onClick={closeMenu}>Projects</a></li>
+                    <li><a href="#contact" onClick={closeMenu}>Contact</a></li>
                 </ul>
 
                 <Button  text="Contact Me" href="#contact" type="primary"/>
             
-                <div className="hamburger">
+                <div className={`hamburger ${isOpen ? "active" : ""}`} onClick={toggleMenu}>
                     <span className="line"></span>
                     <span className="line"></span>
                     <span className="line"></span>
